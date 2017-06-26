12feb1a5148e828ef720ef1cb4c3a259a4ebc7b8
Charles Davison <charlie@powmedia.co.uk>
2017-06-26 13:04:11 +0200

    Fix eslint err

--- dist/wurd.js
+++ dist/wurd.js
@@ -294,7 +294,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; // eslint-disable-line no-unused-vars
+
 
 var _react = __webpack_require__(1);
 
